612909ccfc7692df79331ca1b0a361b3e423b9bf
binjunkai <1176302809@qq.com>
2023-03-21 12:09:24 +0800

    Build project framework

---
+++ api/src/test/java/top/pi1grim/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.pi1grim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
